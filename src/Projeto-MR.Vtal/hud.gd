e2545ebdd66e451beb4e9db38a91f4c0b7830c0c
EverFelliphe <123915570+EverFelliphe@users.noreply.github.com>
2023-03-09 15:49:55 -0300

    update

--- src/Projeto-MR.Vtal/hud.gd
+++ src/Projeto-MR.Vtal/hud.gd
@@ -1,12 +1,38 @@
 extends Node2D
 onready var nivel_imag = $VBoxContainer/nivel
+func _ready():
+	$VBoxContainer/pont_0.show()
 
 func _process(delta):
 	if Global.pontuacao == 1:
-		nivel_imag.texture = load("res://imagens/hud/pixil-frame-0 (8).png")
+		$VBoxContainer/pont_1.show()
+		$VBoxContainer/pont_2.hide()
+		$VBoxContainer/pont_0.hide()
+		$VBoxContainer/pont3.hide()
+		$VBoxContainer/pont_4.hide()
 	elif Global.pontuacao == 2:
-		nivel_imag.texture = load("res://imagens/hud/pixil-frame-0 (6).png")
+		$VBoxContainer/pont_2.show()
+		$VBoxContainer/pont_1.hide()
+		$VBoxContainer/pont_0.hide()
+		$VBoxContainer/pont3.hide()
+		$VBoxContainer/pont_4.hide()
 	elif Global.pontuacao == 3:
-		nivel_imag.texture = load("res://imagens/hud/pixil-frame-0 (7).png")
+		$VBoxContainer/pont3.show()
+		$VBoxContainer/pont_2.hide()
+		$VBoxContainer/pont_1.hide()
+		$VBoxContainer/pont_0.hide()
+		$VBoxContainer/pont_4.hide()
 	elif Global.pontuacao == 4:
-		nivel_imag.texture = load("res://imagens/hud/pixil-frame-0 (8).png")
+		$VBoxContainer/pont_4.show()
+		$Timer.start()
+		$VBoxContainer/pont_1.hide()
+		$VBoxContainer/pont_0.hide()
+		$VBoxContainer/pont3.hide()	
+		$VBoxContainer/pont_2.hide()
+		
+func _on_Timer_timeout():
+	$VBoxContainer/pont_4.hide()
+	$VBoxContainer/pont_0.show()
+	$VBoxContainer/pont_1.hide()
+	$VBoxContainer/pont3.hide()
+	$VBoxContainer/pont_2.hide()
